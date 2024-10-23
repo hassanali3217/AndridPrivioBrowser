054a01a16f501161b419156dbe974931a0c2b0ed
hassanAliJahangir <hassanali3217@gmail.com>
2024-10-23 12:33:42 +0500

    initial commit

--- app/src/main/java/com/astraleratech/priviobrowser/features/wheather/WeatherRepository.kt
+++ app/src/main/java/com/astraleratech/priviobrowser/features/wheather/WeatherRepository.kt
@@ -1,14 +1,15 @@
 package com.astraleratech.priviobrowser.features.wheather
 
+import com.astraleratech.priviobrowser.utils.Keys
 import com.astraleratech.priviobrowser.utils.RetrofitInstance
 
 class WeatherRepository {
 
     // Function to fetch weather data
-    suspend fun getWeatherData(lat:Double , lng :Double): WeatherResponseDM {
+    suspend fun getWeatherData(lat: Double, lng: Double): WeatherResponseDM {
         val latitude = lat
         val longitude = lng
-        val apiKey = "0d6c4b462264bfa47d3a9f4276d38fd5"
+        val apiKey = Keys.wheatherAPiKEy
         return RetrofitInstance.api.getWeatherData(latitude, longitude, apiKey)
     }
 }
---
+++ app/src/main/java/com/astraleratech/priviobrowser/utils/Keys.kt
@@ -0,0 +1,5 @@
+package com.astraleratech.priviobrowser.utils
+
+object Keys {
+    val wheatherAPiKEy = "0d6c4b462264bfa47d3a9f4276d38fd5"
+}
